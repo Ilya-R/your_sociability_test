73af6b0df0e56564a264e05e7dd6d0fd4fac7a44
Ilya-R <megabonus777@mail.ru>
2017-06-18 15:36:00 +1000

    правки по кодстилю

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-require_relative "test.rb"
-require_relative "result.rb"
+require_relative 'test.rb'
+require_relative 'result.rb'
 
 ver = "Ваш уровень общительности. Тест поможет определить ваш уровень коммуникабельности. Версия 1.0\n\n"
 puts ver
